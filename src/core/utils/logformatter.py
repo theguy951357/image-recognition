88eedc67184a762cb739e1bb5b87fe6f6fa34557
Harry Burnett <hburnett777@gmail.com>
2022-02-07 21:21:27 -0500

    Log formatter

---
+++ src/core/utils/logformatter.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class LogFormatter(logging.Formatter):
+    """Formats log output to uniform format."""
+    grey = "\x1b[38;2;170;170;170m"
+    white = "\x1b[38;2;236;236;236m"
+    yellow = "\x1b[38;2;200;200;50;1m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    log_fmt = "%(asctime)s - %(name)s - [%(levelname)s]: %(message)s @(%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + log_fmt + reset,
+        logging.INFO: white + log_fmt + reset,
+        logging.WARNING: yellow + log_fmt + reset,
+        logging.ERROR: red + log_fmt + reset,
+        logging.CRITICAL: bold_red + log_fmt + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
