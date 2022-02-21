2fa5970ba89644ff0b8f28b18e398a4fd5b9f0d7
Harry Burnett <hburnett777@gmail.com>
2022-02-21 16:04:18 -0500

    Renamed 'train' to 'train_dir'

--- src/core/config.py
+++ src/core/config.py
@@ -3,23 +3,23 @@ from enum import Enum
 
 
 class Config:
-    def __init__(self, image, model, name, train, out='./out/', epochs=10):
+    def __init__(self, image, model, name, train_dir, out='./out/', epochs=10):
         self.name = name
         self.out = out
-        self.train = train
+        self.train_dir = train_dir
         self.epochs = epochs
         self.model = model
         self.image = image
 
         # Bool casts due to python short-circuiting None-types.
-        self.is_image_mode = bool(self.image and not self.train)
-        self.is_train_mode = bool(self.train and not self.image)
+        self.is_image_mode = bool(self.image and not self.train_dir)
+        self.is_train_mode = bool(self.train_dir and not self.image)
         if self.is_image_mode == self.is_train_mode:
             raise ValueError('Invalid config. Training mode and image mode must be opposite. '
                              f'Current configuration: Image Mode: {self.is_image_mode} | Train Mode: {self.is_train_mode}')
 
     def __repr__(self):
-        return f'Config(image={self.image}, model={self.model}, name={self.name}, train={self.train}, ' \
+        return f'Config(image={self.image}, model={self.model}, name={self.name}, train={self.train_dir}, ' \
                f'epochs={self.epochs}, out={self.out}) | [Image Mode: {self.is_image_mode}]'
 
     @classmethod
@@ -29,4 +29,4 @@ class Config:
         :param p: The parsed args
         :return: Config
         """
-        return Config(image=p.image, model=p.model, name=p.name, out=p.out, train=p.train, epochs=p.epochs)
+        return Config(image=p.image, model=p.model, name=p.name, out=p.out, train_dir=p.train, epochs=p.epochs)
