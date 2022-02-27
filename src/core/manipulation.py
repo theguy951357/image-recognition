99c8ca257c2dbdb80ee3ba4728aaa8ef738c7697
Harry Burnett <hburnett777@gmail.com>
2022-02-27 17:18:41 -0500

    Added redundancy check for 'eta install models'

--- src/core/manipulation.py
+++ src/core/manipulation.py
@@ -1,19 +1,35 @@
-# Functions for image manipulation, wrapped around FiftyOne
+import logging
 import fiftyone as fo
 import fiftyone.zoo as foz
 import fiftyone.utils.annotations as foua
+
+logger = logging.getLogger(__name__)
 
 
 class Manipulator:
     def __init__(self, config):
         self.config = config
-        self.model = foz.load_zoo_model("centernet-resnet50-v2-512-coco-tf2")
+        self.model = self.load_model()
         self.dataset = fo.Dataset.from_dir(
             dataset_dir=config.image_dir,
             dataset_type=fo.types.ImageDirectory
         )
 
         self.dataset.apply_model(self.model)
+
+    def load_model(self, attempted=False):
+        try:
+            return foz.load_zoo_model("centernet-resnet50-v2-512-coco-tf2")
+        except (ModuleNotFoundError, ImportError) as e:
+            if attempted:
+                raise e
+
+            logger.warning(f'Exception handled: {type(e)} -- Attempting to install eta models.')
+
+            import os
+            os.system('eta install models')
+
+            return self.load_model(True)
 
     def export(self):
         cfg = foua.DrawConfig(
@@ -25,4 +41,3 @@ class Manipulator:
             }
         )
         self.dataset.draw_labels(self.config.out_dir, label_fields=None, config=cfg)
-
